3dbc0452e02306357702e73321e2931ecb987b36
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-11-24 20:22:24 +0100

    clear_code

--- data/data.js
+++ data/data.js
@@ -1,44 +1,62 @@
-module.exports=[
+module.exports = [
   {
     "id": 1,
     "title": "Naruto",
     "author": "Manga Studio Ghibli",
     "img": "/img/naruto.jpeg",
-    "description": "Follow the journey of Naruto Uzumaki, a ninja with dreams of becoming the Hokage and earning acceptance from his peers."
+    "description": "Follow the journey of Naruto Uzumaki, a ninja with dreams of becoming the Hokage and earning acceptance from his peers.",
+    "category": [
+      "anime",
+      "hobby",
+      "sport"
+    ]
   },
   {
     "id": 2,
     "title": "One Punch Man",
     "author": "ONE",
     "img": "/img/one_punch_man.jpeg",
-    "description": "The story follows Saitama, a superhero who is so powerful that he defeats evildoers single-handedly with ease."
+    "description": "The story follows Saitama, a superhero who is so powerful that he defeats evildoers single-handedly with ease.",
+    "category": [
+      "anime",
+      "hobby",
+      "sport"
+    ]
   },
   {
     "id": 3,
     "title": "My Hero Academia (MHA)",
     "author": "Kosuke Mikami",
     "img": "/img/my_hero.jpeg",
-    "description": "Set in a world where everyone has some form of superpower, only those with 'Quirks' are allowed into Izu University. For one boy who was born without any Quirk and not accepted by society..."
+    "description": "Set in a world where everyone has some form of superpower, only those with 'Quirks' are allowed into Izu University. For one boy who was born without any Quirk and not accepted by society...",
+    "category": [
+      "anime",
+      "hobby",
+      "sport"
+    ]
   },
   {
     "id": 4,
     "title": "Fullmetal Alchemist: Brotherhood",
     "author": "Hajime Egusa & Yousuke Kurokiba",
     "img": "/img/full_metal.jpeg",
-    "description": "Two brothers seek to find the secret to alchemy, but their quest has grave consequences."
+    "description": "Two brothers seek to find the secret to alchemy, but their quest has grave consequences.",
+    "category": [
+      "anime",
+      "hobby",
+      "sport"
+    ]
   },
   {
     "id": 5,
     "title": "one piece",
     "author": "Luffy",
     "img": "/img/one_piece.jpeg",
-    "description": "One Piece (stylized in all caps) is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump since July 1997, with its chapters compiled in 110 tankōbon volumes as of November 2024. The series follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, as he explores the Grand Line in search of the mythical treasure known as the"
-  },
-  {
-    "id": 6,
-    "title": "dragonball",
-    "author": "luigi serra",
-    "img": "/img/full_metal.jpeg",
-    "description": "anime violento"
+    "description": "One Piece (stylized in all caps) is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump since July 1997, with its chapters compiled in 110 tankōbon volumes as of November 2024. The series follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, as he explores the Grand Line in search of the mythical treasure known as the",
+    "category": [
+      "anime",
+      "hobby",
+      "sport"
+    ]
   }
 ]
